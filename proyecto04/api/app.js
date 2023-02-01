e4f706ebb1cb524e1f906ccb93de464bf4be9cf9
Gustavo-Eduardo <guedlope@espol.edu.ec>
2023-01-31 22:31:45 -0500

    api middleware to avoid cors

--- proyecto04/api/app.js
+++ proyecto04/api/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { corsMiddleware } = require("./middlewares")
 
 require("dotenv").config()
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(corsMiddleware)
 app.use('/', indexRouter);
 
 module.exports = app;
---
+++ proyecto04/api/middlewares/index.js
@@ -0,0 +1,8 @@
+exports.corsMiddleware = (req, res, next) => {
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
+    });
+    next();
+};
